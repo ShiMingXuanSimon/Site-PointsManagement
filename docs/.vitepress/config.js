b46314f5a171fb474eb00506bba1b6014c18decd
ShiMingXuanSimon <1923696262@qq.com>
2026-02-06 22:34:03 +0800

    增加主站链接

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ export default {
   description: 'Points Management 官方网站',
   themeConfig: {
     nav: [
+      { text: 'SMXS的主页', link: 'https://www.smx-s.com/' },
       { text: '首页', link: '/' },
       { text: '文档', link: '/docs/' },
       { text: '下载', link: '/download/' }
