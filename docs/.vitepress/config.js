3bc67f44d83d90d9b4bc7f3cea2ec7c90445e8b5
ShiMingXuanSimon <1923696262@qq.com>
2026-02-06 15:43:16 +0800

    添加页眉

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+  title: '积分管理系统',
+  description: 'Points Management 官方网站',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/docs/' },
+      { text: '下载', link: '/download/' }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ShiMingXuanSimon/Site-PointsManagement' }
+    ]
+  }
+}
